c8e8b53c4ca0c6c785496e7292265b98269f3602
tomsennett-sga <91488588+tomsennett-sga@users.noreply.github.com>
2021-10-25 17:25:02 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,11 @@ module.exports = {
               perPage: 20, // currently set to 100
               requestConcurrency: 5, // currently set to 15
               previewRequestConcurrency: 2, // currently set to 5
-            }
+            },
+          includedRoutes: [
+                  "**/entertainers",
+                  "**/posts",
+                ]
       },
     },
 
